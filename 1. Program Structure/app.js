0d7f1106a05fa26d07ff093edb0889f43a702367
Bibek Dhungana <Dhunganabibek2016@gmail.com>
2022-09-12 12:33:44 -0500

    Question number 1 Completed

--- 1. Program Structure/app.js
+++ 1. Program Structure/app.js
@@ -8,6 +8,17 @@
     ######
     #######
  */
+console.log("Question Number 1: Pattern");
+const printTriangle = (n) => {
+    for(let i = 1; i <= n; i++){
+        for(let j = 1; j <= i; j++){
+            process.stdout.write('#');
+        }
+        process.stdout.write('\n');
+    }
+}
+
+printTriangle(7)
 
 /*
     2. Write a program that creates a string that represents an 8×8 grid, 
@@ -15,3 +26,19 @@
     At each position of the grid there is either a space or a "#" character. 
     The characters should form a chessboard.
 */
+console.log("Question Number 2: chessboard");
+const chessboard = (n) => {
+    for(let i = 1; i <= n; i++ ){
+        for(let j = 0; j <= n; j++){
+            if(((i + j) % 2 === 0)){
+                process.stdout.write(' ');
+            }
+            else{
+                process.stdout.write('#');
+            }
+        }
+        process.stdout.write('\n');
+    }
+}
+
+chessboard(8);
